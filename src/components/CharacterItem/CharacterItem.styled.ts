7d871d3424f4fc150e9bd35a85937dac02369ef7
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-03-23 20:10:47 +0200

    fix margin

--- src/components/CharacterItem/CharacterItem.styled.ts
+++ src/components/CharacterItem/CharacterItem.styled.ts
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
 export const CharactersItem = styled.li`
+  @media ${(p) => p.theme.media.mobileM} {
+    margin-bottom: 32px;
+  }
   background: #ffffff;
   overflow: hidden;
   box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.2), 0px 3px 4px rgba(0, 0, 0, 0.12),
     0px 2px 4px rgba(0, 0, 0, 0.14);
   border-radius: 4px;
-  margin-bottom: 32px;
   transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
   &:hover,
   &:focus {
